8e8561466e0a31bffd071685c1fd7ee7eb69b04c
Paulo Campos <121526199+PauloChamps@users.noreply.github.com>
2026-01-25 09:17:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Script básico inicial
+// Aqui você pode futuramente adicionar:
+// - menu mobile
+// - carregamento de artigos
+// - banners dinâmicos
+// - analytics
+
+console.log("Tudo Sobre Rodas carregado com sucesso!");
