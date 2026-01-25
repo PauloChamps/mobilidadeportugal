13995b00267e97e080f2f568fde341f554bbf22b
Paulo Campos <121526199+PauloChamps@users.noreply.github.com>
2026-01-25 18:31:42 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,31 @@
 // - analytics
 
 console.log("Tudo Sobre Rodas carregado com sucesso!");
+
+
+
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    const body = document.body;
+
+    sidebar.classList.toggle('open');
+
+    if (sidebar.classList.contains('open')) {
+        body.style.overflow = 'hidden'; // trava scroll
+    } else {
+        body.style.overflow = ''; // libera scroll
+    }
+}
+
+/* Fecha o menu ao clicar em qualquer link */
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebar = document.getElementById('sidebar');
+    const links = sidebar.querySelectorAll('a');
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            sidebar.classList.remove('open');
+            document.body.style.overflow = '';
+        });
+    });
+});
